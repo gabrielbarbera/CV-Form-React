1c1e30c96f0f9b64782d4046f67e2042053db8af
gandalfmza <gabrielbarbera@gmail.com>
2018-11-29 18:38:23 -0300

    Add files via upload

---
+++ views/JobsFormView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormJobs from '../components/FormJobs/FormJobs';
+
+ReactDOM.render(
+	<FormJobs />,
+document.getElementById("jobs-form"));
